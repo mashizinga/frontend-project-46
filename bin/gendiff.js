6234a7e129ba5f62f0e4db1ae053fc569021707f
mashizinga <maryzakharova0103@gmail.com>
2025-08-20 14:59:29 +0300

    arguments and option

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -5,6 +5,9 @@ program
     .name('gendiff')
     .description('Compares two configuration files and shows a difference.')
     .version('1.0.0', '-V, --version', 'output the version number')
+    .argument('<filepath1>')
+    .argument('<filepath2>')
+    .option('-f, --format [type]', 'output format')
     .helpOption('-h, --help', 'display help for command')
 
     program.parse()
