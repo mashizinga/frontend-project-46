e5e92b3a4a0e8445f26b267c9fd5088c4eaa480e
mashizinga <maryzakharova0103@gmail.com>
2025-08-25 17:07:36 +0300

    new dir tests

--- tests/diff.test.js
+++ tests/diff.test.js
@@ -6,9 +6,11 @@ import { test, expect } from '@jest/globals'
 const __filename = fileURLToPath(import.meta.url)
 const __dirname = path.dirname(__filename)
 
+const getFixturePath = filename => path.join(__dirname, '..', '__fixtures__', filename)
+
 test('gendiff compares two JSON files correctly', () => {
-  const filepath1 = path.join(__dirname, 'file1.json')
-  const filepath2 = path.join(__dirname, 'file2.json')
+  const filepath1 = getFixturePath('file1.json')
+  const filepath2 = getFixturePath('file2.json')
 
   const expected = `{
   - follow: false
